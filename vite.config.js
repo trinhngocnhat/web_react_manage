3ffcdba7eb2bbdaa568f5b60f996a83c03f26c07
trinhngocnhat <trinhngocnhat079@gmail.com>
2025-08-15 21:46:13 +0700

    config taiwind

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import "tailwindcss"
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [react(), "tailwindcss"],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
